e92253efb825721d13c4a94cead1bb68defc6c07
sksn <ps.net26@gmail.com>
2023-05-05 10:51:32 +0900

    cahnge footer

--- nextjs-hasura-app/src/components/Layout/index.tsx
+++ nextjs-hasura-app/src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode, FC } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import style from 'src/components/Layout/index.module.css'
 
 interface Props {
   children: ReactNode
@@ -35,15 +36,25 @@ export const Layout: FC<Props> = ({ children, title = "dOwOd's logs" }) => {
         <main className="container">{children}</main>
 
         <footer>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            {/* <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" /> */}
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </a>
+          <div className={style.footer}>
+          <div className="grid">
+            <small>© 2023 dowod.dev</small>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{' '}
+              {/* <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" /> */}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </a>
+          </div>
+          </div>
         </footer>
       </article>
     </div>
