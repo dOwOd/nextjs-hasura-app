18a736536d288e73d3fc9ab2dee0f7bad36ee883
sksn <ps.net26@gmail.com>
2023-05-15 23:42:36 +0900

    remove next-fortress

--- nextjs-hasura-app/src/pages/articles/_middleware.ts
+++
@@ -1,8 +0,0 @@
-import { makeIPInspector } from 'next-fortress'
-
-// 第一引数: 許可するIPアドレス。CIDR形式、および配列で複数のIP指定も可
-// 第二引数: 許可IPレンジ外からのアクセスに対しての制御ルール。詳しくはREADMEをどうぞ。https://github.com/aiji42/next-fortress#usage
-export const middleware = makeIPInspector(process.env.NEXT_PUBLIC_IP_ADDRESS || '', {
-  type: 'redirect',
-  destination: '/'
-})
