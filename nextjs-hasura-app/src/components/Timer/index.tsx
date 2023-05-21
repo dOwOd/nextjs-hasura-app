9bf0111225475af877bd0d7e50e8e60f835b2cd9
sksn <ps.net26@gmail.com>
2023-05-21 16:57:21 +0900

    Timerコンポーネントを作成

---
+++ nextjs-hasura-app/src/components/Timer/index.tsx
@@ -0,0 +1,27 @@
+import { FC, useEffect, useState } from 'react'
+
+export const Timer: FC = () => {
+  const [date, setDate] = useState('')
+
+  const formatDate = (hour: number, minute: number, second: number, millisecond: number) => {
+    setDate(`${hour}:${minute}:${second} ${millisecond}`)
+  }
+  
+  useEffect(() => {
+    setInterval(() => {
+      const nowDate = new Date()
+      const hour = nowDate.getHours()
+      const minute = nowDate.getMinutes()
+      const second = nowDate.getSeconds()
+      const millisecond = nowDate.getMilliseconds()
+      formatDate(hour, minute, second, millisecond)
+    }, 10)
+  }, [date])
+
+
+  return (
+    <>
+      {date}
+    </>
+  )
+}
