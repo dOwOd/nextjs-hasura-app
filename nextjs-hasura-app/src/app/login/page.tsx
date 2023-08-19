57df93342e96883283386ad2603060cbf2e2c53d
sksn <ps.net26@gmail.com>
2023-08-19 16:56:04 +0900

    検証用

--- nextjs-hasura-app/src/app/login/page.tsx
+++ nextjs-hasura-app/src/app/login/page.tsx
@@ -7,23 +7,16 @@ import { SeeYou } from 'src/components/SeeYou'
 
 const Page = () => {
   const { data: session } = useSession()
-  const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://www.dowo.dev/'
 
-  if (session == undefined) {
-    console.log(baseUrl);
-    
+  if (session == undefined)
     return (
       <Layout>
-        <button onClick={() => signIn('github', { callbackUrl: `${baseUrl}articles` })}>Sign in</button>
+        <button onClick={() => signIn('github')}>Sign in</button>
       </Layout>
     )
-  }
-
-  console.log(session.user?.email);
-  console.log(isVerifyEmailAddress(session.user?.email));
 
   const detail = isVerifyEmailAddress(session.user?.email) ? (
-    window.location.href = `${baseUrl}articles`
+    (window.location.href = '/articles')
   ) : (
     <SeeYou />
   )
--- nextjs-hasura-app/src/pages/api/auth/[...nextauth].ts
+++ nextjs-hasura-app/src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,21 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { isVerifyEmailAddress } from 'src/lib/utils/isVerifyEmailAddress'
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || ''
-    })
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
   ],
   session: {
     strategy: 'jwt',
     maxAge: 60 * 60 * 24 * 30,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      return isVerifyEmailAddress(user.email)
+    },
   },
 })
