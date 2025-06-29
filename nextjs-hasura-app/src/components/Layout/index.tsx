6fdfc22bf84c7a73d67a4245746936eeca6faed5
dowod <l.doaod@gmail.com>
2025-06-30 08:17:03 +0900

    fix fotter font size

--- nextjs-hasura-app/src/components/Layout/index.tsx
+++ nextjs-hasura-app/src/components/Layout/index.tsx
@@ -42,6 +42,7 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => (
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
+            className={style.poweredByLink}
           >
             Powered by{' '}
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
